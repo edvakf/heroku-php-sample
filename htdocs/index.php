c734690247271e943b2877584504bef814fc198f
Atsushi Takayama <taka.atsushi@gmail.com>
2014-04-23 02:49:44 +0900

    make index.php slim-based

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,1 +1,13 @@
-<?php phpinfo();
+<?php
+date_Default_timezone_set('Asia/Tokyo');
+define('ROOT_DIR', dirname(__DIR__));
+
+require ROOT_DIR.'/vendor/composer/autoload.php';
+
+$app = new \Slim\Slim;
+
+$app->get('/', function() use ($app) {
+  phpinfo();
+});
+
+$app->run();
