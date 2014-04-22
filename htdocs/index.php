cf009759166a813a6612cb4e349c4d3b95ff9181
Atsushi Takayama <taka.atsushi@gmail.com>
2014-04-23 03:07:48 +0900

    change composer.json setting to put composer installed files under vendor/composer

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,7 +2,7 @@
 date_Default_timezone_set('Asia/Tokyo');
 define('ROOT_DIR', dirname(__DIR__));
 
-require ROOT_DIR.'/vendor/autoload.php';
+require ROOT_DIR.'/vendor/composer/autoload.php';
 
 $app = new \Slim\Slim;
 
