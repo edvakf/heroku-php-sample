dc1911cd6eceee44e103172fe74ae3de3e274ab2
Atsushi Takayama <taka.atsushi@gmail.com>
2014-04-23 03:06:17 +0900

    remove debug print

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,21 +1,13 @@
 <?php
-echo 1;
 date_Default_timezone_set('Asia/Tokyo');
-echo 2;
 define('ROOT_DIR', dirname(__DIR__));
-echo 3;
 
 require ROOT_DIR.'/vendor/autoload.php';
-echo 4;
 
 $app = new \Slim\Slim;
-echo 5;
 
 $app->get('/', function() use ($app) {
-echo 8;
   phpinfo();
 });
-echo 6;
 
 $app->run();
-echo 7;
