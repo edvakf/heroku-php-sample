2dabb175eeda03db490687c300c50674ceb5e7d5
Atsushi Takayama <taka.atsushi@gmail.com>
2014-04-24 10:21:09 +0900

    use PDO to connect to cleardb mysql

--- htdocs/index.php
+++ htdocs/index.php
@@ -17,6 +17,14 @@ $app->get('/', function() use ($app) {
     $app->render('index.tpl', array('phpinfo' => $phpinfo));
 });
 
+$app->get('/mysql', function() use ($app) {
+
+    $row = $conn->query("SELECT 1")->fetch(PDO::FETCH_ASSOC);
+
+    var_dump($row);
+
+});
+
 $app->run();
 
 function getPhpInfo()
@@ -26,4 +34,15 @@ function getPhpInfo()
     $phpinfo = ob_get_contents();
     ob_end_clean();
     return $phpinfo;
+}
+
+function getPDOConnection()
+{
+    $cleardb = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $conn = new PDO(
+        sprintf("mysql:dbname=%s;host=%s", substr($cleardb['path'], 1), $cleardb['host']),
+        $cleardb['user'],
+        $cleardb['pass']
+    );
+    return $conn;
 }
